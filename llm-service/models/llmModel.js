c3dc4b1ba3af6431ff2591e4ad7f12a3bd26a3da
MaxtonN <rustdread@gmail.com>
2025-10-30 13:56:01 -0400

    Added model format

---
+++ llm-service/models/llmModel.js
@@ -0,0 +1,14 @@
+
+
+/*
+ * Communicates with ChatGPT API to parse text and send back the parsed information
+ *
+ * message -> string, message that ChatGPT will parse
+ * 
+ * return: string list, ChatGPT response to all queries
+ */
+const Parse = async (message) => {
+    
+}
+
+module.exports = { Parse };
