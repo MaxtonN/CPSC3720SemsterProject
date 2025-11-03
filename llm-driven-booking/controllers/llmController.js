78af44765c3b2ff8526062baa352982f9d791674
MaxtonN <rustdread@gmail.com>
2025-11-02 21:12:24 -0500

    LLM active

--- llm-driven-booking/controllers/llmController.js
+++ llm-driven-booking/controllers/llmController.js
@@ -87,8 +87,8 @@ const parseText = async (req, res) => {
         let parsedInformation = JSON.parse('{"intent":null, "ticket_amount":null, "event_name":null}');
         let counter = 2;
         while((!parsedInformation || !parsedInformation.intent || !parsedInformation.ticket_amount || !parsedInformation.event_name) && counter > 0){
-            //const response = await Parse(req.body.message);
-            //parsedInformation = JSON.parse(response);
+            const response = await Parse(req.body.message);
+            parsedInformation = JSON.parse(response);
             counter = counter - 1;
         }
 
