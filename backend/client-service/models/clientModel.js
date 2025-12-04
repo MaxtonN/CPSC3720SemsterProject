6b9246c386106fbc48b94d52ad31a5a04f54a269
MaxtonN <rustdread@gmail.com>
2025-12-03 21:21:43 -0500

    Update path

--- backend/client-service/models/clientModel.js
+++ backend/client-service/models/clientModel.js
@@ -1,7 +1,7 @@
 const Database = require("better-sqlite3");
 //const databaseFilePath = "shared-db/database.sqlite";
 const path = require("path");
-const databaseFilePath = path.join(__dirname, '..', 'shared-db', 'database.sqlite');
+const databaseFilePath = path.join(__dirname, '..', '..', 'shared-db', 'database.sqlite');
 /*
  * getEvent retrieves and returns the row indicated by eventId from the events table of the shared database (database.sqlite)
  *
